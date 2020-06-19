9e208c94246cb0e9f327959cc3b990e9f909964a
unknown <acemonkey2@gmail.com>
2020-06-19 11:14:24 -0700

    Updates

---
+++ precache-manifest.4a102d3d275765c22ce4a55984f4047a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faf483ea3e3b888edd57dab67e3fff62",
+    "url": "/BLOCKCHAIN-MUSIKA/index.html"
+  },
+  {
+    "revision": "de0b21bcdc1d0e0b36ec",
+    "url": "/BLOCKCHAIN-MUSIKA/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3382be87dcd953c86a29",
+    "url": "/BLOCKCHAIN-MUSIKA/static/css/main.3f740955.chunk.css"
+  },
+  {
+    "revision": "de0b21bcdc1d0e0b36ec",
+    "url": "/BLOCKCHAIN-MUSIKA/static/js/2.6607efdf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BLOCKCHAIN-MUSIKA/static/js/2.6607efdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3382be87dcd953c86a29",
+    "url": "/BLOCKCHAIN-MUSIKA/static/js/main.4c03b417.chunk.js"
+  },
+  {
+    "revision": "474ee58e0b68fe35bfd6",
+    "url": "/BLOCKCHAIN-MUSIKA/static/js/runtime-main.5f0a102a.js"
+  },
+  {
+    "revision": "66fee598da905dfd40cfaa7ba0bb78a6",
+    "url": "/BLOCKCHAIN-MUSIKA/static/media/darktheme.66fee598.jpg"
+  },
+  {
+    "revision": "d7e45ded8c47337dd3c2abe2d2614c5d",
+    "url": "/BLOCKCHAIN-MUSIKA/static/media/head.d7e45ded.png"
+  },
+  {
+    "revision": "4ea1e6f07ac32697badd29518cfff42b",
+    "url": "/BLOCKCHAIN-MUSIKA/static/media/head1.4ea1e6f0.jpg"
+  },
+  {
+    "revision": "5076bdfe2ac27042d8eb19865f24e483",
+    "url": "/BLOCKCHAIN-MUSIKA/static/media/headset.5076bdfe.png"
+  },
+  {
+    "revision": "bf2aea39e9db69cf6e5b62d3f5f11c26",
+    "url": "/BLOCKCHAIN-MUSIKA/static/media/icon.bf2aea39.jpg"
+  },
+  {
+    "revision": "3d52676a3be4b371e26498492a57baed",
+    "url": "/BLOCKCHAIN-MUSIKA/static/media/lemon.3d52676a.jpg"
+  },
+  {
+    "revision": "75aacd8b58fb1cd86d772727874441b4",
+    "url": "/BLOCKCHAIN-MUSIKA/static/media/mm1.75aacd8b.jpg"
+  },
+  {
+    "revision": "96ab1591317fc85cc9d5cd1a3530a88f",
+    "url": "/BLOCKCHAIN-MUSIKA/static/media/mm2.96ab1591.jpg"
+  },
+  {
+    "revision": "c3c7b3671f53f15701a086d95eca03a4",
+    "url": "/BLOCKCHAIN-MUSIKA/static/media/mm3.c3c7b367.jpg"
+  },
+  {
+    "revision": "618a795d23df5b546eb015d0e0a144d8",
+    "url": "/BLOCKCHAIN-MUSIKA/static/media/mm4.618a795d.jpg"
+  },
+  {
+    "revision": "69567356e2f84f1b73ef7cea135079e1",
+    "url": "/BLOCKCHAIN-MUSIKA/static/media/song1edsheeran.69567356.jpg"
+  },
+  {
+    "revision": "d5468eab060564b54146ee03b5518d8d",
+    "url": "/BLOCKCHAIN-MUSIKA/static/media/song2brokenheart.d5468eab.jpg"
+  },
+  {
+    "revision": "54477c0f4e52035e27a559c56b440ec2",
+    "url": "/BLOCKCHAIN-MUSIKA/static/media/song3dualipa.54477c0f.jpg"
+  },
+  {
+    "revision": "67683777fc945ee8c41d20f11e492ca8",
+    "url": "/BLOCKCHAIN-MUSIKA/static/media/song4revamp.67683777.jpg"
+  },
+  {
+    "revision": "9bd636d134316a86f93ff712b77c1015",
+    "url": "/BLOCKCHAIN-MUSIKA/static/media/song5eclipse.9bd636d1.jpg"
+  },
+  {
+    "revision": "be021dae6ef325f033c40bf288b3455e",
+    "url": "/BLOCKCHAIN-MUSIKA/static/media/song6katy.be021dae.jpg"
+  }
+]);
--- src/components/404/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const NotFound = (props) => {
-  return (
-    <React.Fragment>
-    	<h1>404 Not Found</h1>
-    </React.Fragment>
-  )
-}
